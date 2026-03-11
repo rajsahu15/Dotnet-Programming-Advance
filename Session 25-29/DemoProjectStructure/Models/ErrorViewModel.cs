3536391902e24ea987861fa0f2084a167660012d
rajsahu15 <rajsahuwork@gmail.com>
2026-03-11 13:29:42 +0900

    project structure

---
+++ Session 25-29/DemoProjectStructure/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DemoProjectStructure.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
